6caa69366e084d525d8fc44412f6898cfd8d8dde
jai-vignesh007 <ravichandran.ja@northeastern.edu>
2025-02-26 01:36:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "this" {
 
 # Creating the Internet Gateway and attach it to the VPC
 resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
+  vpc_id = aws_vpc.this.bad
 
   tags = {
     Name = "${var.vpc_name}-igw"
